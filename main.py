09ff10a22621e2d94d56a4e8191cf7b37c364c51
Aleksandar Krsteski <aleksandar.krsteski@vorteksed.com.mk>
2022-08-22 15:03:01 +0200

    Store petrol price

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    f = open("petrol_price.txt", 'r')
+    petrol_price = float(f.read() or 0)
+
     return templates.TemplateResponse(
-        "index.html", {"request": request})
+        "index.html", {
+            "request": request,
+            "petrol_price": petrol_price
+        }
+    )
 
 
 @app.post("/calculate", response_class=HTMLResponse)
@@ -30,14 +37,26 @@ async def calculate(
     driven_time: time = Form(),
     arrive_dt: datetime = Form()
 ):
+    err_msg = ""
+
     if (start_odometer is None and driven_km is None) or (
             start_odometer is not None and driven_km is not None):
-        return templates.TemplateResponse(
-            "missing_fields.html", {"request": request}
-        )
+
+        err_msg = "Required 'start_odometer' or 'driven_km'"
 
     if driven_km:
         start_odometer = arrive_odometer - driven_km
+
+    if arrive_odometer < start_odometer:
+        err_msg = f"'arrive_odometer' must be greated then {start_odometer}"
+
+    if err_msg != "":
+        return templates.TemplateResponse(
+            "missing_fields.html", {
+                "request": request,
+                "err_msg": err_msg
+            }
+        )
 
     start_dt = arrive_dt - timedelta(
         hours=driven_time.hour,
@@ -48,6 +67,11 @@ async def calculate(
     distance = arrive_odometer - start_odometer
     trip_consumption = round((distance * consumption) / 100, 1)
     cost = round(trip_consumption * petrol_price)
+    cost_by_km = round((cost / distance), 2)
+
+    f = open("petrol_price.txt", 'w')
+    f.write(str(petrol_price))
+    f.close()
 
     return templates.TemplateResponse(
         "calculations.html", {
@@ -56,6 +80,7 @@ async def calculate(
             "trip_consumption": trip_consumption,
             "petrol_price": petrol_price,
             "cost": cost,
-            "start_dt": start_dt
+            "start_dt": start_dt,
+            "cost_by_km": cost_by_km
         }
     )
